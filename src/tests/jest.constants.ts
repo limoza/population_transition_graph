2d39e88b14a66e02138b95179a705dd0131a1599
limoza <svacc.nelllely@gmail.com>
2024-07-27 00:40:23 +0900

    💚 /libの関数にテストを追加

---
+++ src/tests/jest.constants.ts
@@ -0,0 +1,6 @@
+export const HOKKAIDO = { prefCode: 1, prefName: '北海道' };
+export const TOKYO = { prefCode: 13, prefName: '東京' };
+export const OSAKA = { prefCode: 27, prefName: '大阪' };
+export const INITIAL_SELECTED_PREFS_STATE = {
+  selectedPrefs: [HOKKAIDO, TOKYO],
+};
