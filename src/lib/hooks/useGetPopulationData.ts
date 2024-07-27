4fb14ca35ae9c9952985973bd7041ac447080c7d
limoza <svacc.nelllely@gmail.com>
2024-07-27 22:21:20 +0900

    ✨ 人口推移取得処理の作成

---
+++ src/lib/hooks/useGetPopulationData.ts
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+
+export const useGetPopulationData = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          'https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=',
+          {
+            headers: {
+              'X-API-KEY': `${process.env.NEXT_PUBLIC_REASAS_API_KEY}`,
+            },
+          },
+        );
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        setData(result.result);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { data, loading, error };
+};
