587c11fa62bf59d05d5b6a3560aba293c55cce5a
limoza <svacc.nelllely@gmail.com>
2024-07-30 21:20:12 +0900

    🔥 不要なhelperの削除

--- src/lib/helper/handlePopulationCategoryChange.ts
+++
@@ -1,6 +0,0 @@
-import type { PopulationCategoryId } from '@/types';
-
-export const handlePopulationCategoryChange = (
-  setPopulationCategory: React.Dispatch<PopulationCategoryId>,
-  categoryId: string,
-) => setPopulationCategory(categoryId as PopulationCategoryId);
