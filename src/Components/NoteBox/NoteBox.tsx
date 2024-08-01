2ea385b0779e87bf1515cce13629fd2f3c5c706c
limoza <svacc.nelllely@gmail.com>
2024-08-02 01:15:03 +0900

    ✨ NoteBoxコンポーネントの追加

---
+++ src/Components/NoteBox/NoteBox.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { css } from '@linaria/core';
+import { spacing, colors } from '@/styles/variables';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const NoteBox = ({ children }: Props) => (
+  <div className={noteBoxStyle}>{children}</div>
+);
+
+export const noteBoxStyle = css`
+  margin-top: ${spacing[4]};
+  text-align: center;
+  padding: ${spacing[20]};
+  background-color: ${colors.gray[100]};
+`;
