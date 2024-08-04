74e4035cb10619d33b3236675d66384eb5dcca38
limoza <svacc.nelllely@gmail.com>
2024-08-04 13:25:08 +0900

    💄 loadingのスピナーを追加

--- src/Components/LoadingBox/LoadingBox.tsx
+++ src/Components/LoadingBox/LoadingBox.tsx
@@ -2,11 +2,36 @@ import React from 'react';
 import { css } from '@linaria/core';
 import { spacing, colors } from '@/styles/variables';
 
-export const LoadingBox = () => <div className={loadingBox}>Loading...</div>;
+export const LoadingBox = () => (
+  <div className={loadingBox}>
+    <span className={loader}></span>
+  </div>
+);
 
 const loadingBox = css`
   margin-top: ${spacing[4]};
   text-align: center;
   padding: ${spacing[20]} ${spacing[4]};
   background-color: ${colors.gray[100]};
+`;
+
+const loader = css`
+  width: 48px;
+  height: 48px;
+  border: 5px solid ${colors.white};
+  border-bottom-color: ${colors.green[500]};
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: rotate 1s linear infinite;
+
+  @keyframes rotate {
+    from {
+      transform: rotate(0deg);
+    }
+
+    to {
+      transform: rotate(360deg);
+    }
+  }
 `;
