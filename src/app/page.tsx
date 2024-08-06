627c19e0790d1f0c42d0958c7b7f71048def1638
limoza <svacc.nelllely@gmail.com>
2024-08-06 22:05:46 +0900

    ♻️ header要素の追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,9 @@ import { colors, spacing, fontSize } from '@/styles/variables';
 export default function Home() {
   return (
     <main>
-      <h1 className={mainTitle}>都道府県別 人口推移グラフ</h1>
+      <header>
+        <h1 className={mainTitle}>都道府県別 人口推移グラフ</h1>
+      </header>
       <SelectedPrefsProvider>
         <Application />
       </SelectedPrefsProvider>
